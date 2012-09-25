55eee69f7de76d0dba86223c8fd2f7b3337c4454
Thomas Harte <thomas.harte@gmail.com>
2012-09-25 12:12:06 -0700

    Added insert and delete line for the Osborne, plus a quick list of codes I've yet to implement for that terminal. Corresponding lists for the Hazeltine 1500 and the ADM3A will be forthcoming.

--- CPM for OS X/BIOS/TerminalControlSet.m
+++ CPM for OS X/BIOS/TerminalControlSet.m
@@ -531,13 +531,13 @@ typedef struct
 
 	CPMTerminalControlSequenceStruct sequences[] =
 	{
-		{@"\x08",	0,	^{	[weakSelf leftCursor];	}},
-		{@"\x0c",	0,	^{	[weakSelf rightCursor];	}},
-		{@"\x0b",	0,	^{	[weakSelf upCursor];	}},
+		{@"\x08",	0,	^{	[weakSelf leftCursor];	}},	// i.e. ^H
+		{@"\x0c",	0,	^{	[weakSelf rightCursor];	}},	// i.e. ^L
+		{@"\x0b",	0,	^{	[weakSelf upCursor];	}},	// i.e. ^K
 		{@"\x1a",	0,	^{
 							[weakSelf homeCursor];
 							[weakSelf clearToEndOfScreen];
-						}},
+						}},								// i.e. ^Z
 		{@"\x1e",	0,	^{	[weakSelf homeCursor];	}},
 		{@"\33=",	4,	^{
 							[weakSelf
@@ -545,12 +545,30 @@ typedef struct
 									y:(weakSelf.inputQueue[2] - 32)%weakSelf.height];
 						}},
 		{@"\33T",	0,	^{	[weakSelf clearToEndOfLine];	}},
+
 		{@"\33)",	0,	^{	weakSelf.currentAttribute |= kCPMTerminalAttributeReducedIntensityOn;	}},
 		{@"\33(",	0,	^{	weakSelf.currentAttribute &= ~kCPMTerminalAttributeReducedIntensityOn;	}},
 		{@"\33L",	0,	^{	weakSelf.currentAttribute |= kCPMTerminalAttributeUnderlinedOn;			}},
 		{@"\33M",	0,	^{	weakSelf.currentAttribute &= ~kCPMTerminalAttributeUnderlinedOn;		}},
+
+		{@"\33E",	0,	^{	[weakSelf insertLine];			}},
+		{@"\33R",	0,	^{	[weakSelf deleteLine];			}},
 		{nil}
 	};
+
+	/*
+		Unimplemented at present:
+		
+			07		^G		rings the bell
+			1b 23	ESC #	locks keyboard
+			1b 22	ESC "	unlocks keyboard
+			1b 53	ESC S	screen XY positioning
+			1b 51	ESC Q	insert character
+			1b 57	ESC W	delete character
+			1b 67	ESC g	start graphics display
+			1b 47	ESC G	end graphics display
+			1b 5b	ESC [	homes screen
+	*/
 
 	[self installControlSequencesFromStructs:sequences];
 }
