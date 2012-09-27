71ea897c01ab4bd1947acf59b743dea9aa5ebcc5
Thomas Harte <thomas.harte@gmail.com>
2012-09-26 21:52:43 -0700

    Added to the ADM3A (/Kaypro?) stuff those effects that I've just implemented for the VT52 anyway.

--- CPM for OS X/BIOS/TerminalControlSet.m
+++ CPM for OS X/BIOS/TerminalControlSet.m
@@ -482,6 +482,8 @@ typedef struct
 	/*
 		This is actually the pure ADM3A with some Kaypro extensions thrown
 		in, I believe...
+		
+		Update! This may well just be the Kaypro. I'll need to look into this.
 	*/
 	__weak __block typeof(self) weakSelf = self;
 
@@ -502,6 +504,7 @@ typedef struct
 									setCursorX:(weakSelf.inputQueue[3] - 32)%weakSelf.width
 									y:(weakSelf.inputQueue[2] - 32)%weakSelf.height];
 						}},
+
 		{@"\33B0",	0,	^{	weakSelf.currentAttribute |= kCPMTerminalAttributeInverseVideoOn;		}},
 		{@"\33C0",	0,	^{	weakSelf.currentAttribute &= ~kCPMTerminalAttributeInverseVideoOn;		}},
 		{@"\33B1",	0,	^{	weakSelf.currentAttribute |= kCPMTerminalAttributeReducedIntensityOn;	}},
@@ -510,6 +513,13 @@ typedef struct
 		{@"\33C2",	0,	^{	weakSelf.currentAttribute &= ~kCPMTerminalAttributeBlinkingOn;			}},
 		{@"\33B3",	0,	^{	weakSelf.currentAttribute |= kCPMTerminalAttributeUnderlinedOn;			}},
 		{@"\33C3",	0,	^{	weakSelf.currentAttribute &= ~kCPMTerminalAttributeUnderlinedOn;		}},
+
+		{@"\33B4",	0,	^{	weakSelf.cursorIsDisabled = NO;			}},
+		{@"\33C4",	0,	^{	weakSelf.cursorIsDisabled = YES;		}},
+
+		{@"\33B6",	0,	^{	[weakSelf saveCursorPosition];			}},
+		{@"\33C6",	0,	^{	[weakSelf restoreCursorPosition];		}},
+
 		{@"\33R",	0,	^{	[weakSelf deleteLine];	}},
 		{@"\33E",	0,	^{	[weakSelf insertLine];	}},
 		{nil}
@@ -520,9 +530,7 @@ typedef struct
 
 			(the graphics characters, 128–255)
 
-			Cursor visible/invisible           B4/C4
-			Video mode on/off                  B5/C5   
-			Remember/recover cursor position   B6/C6
+			Video mode on/off                  B5/C5
 			Status line preservation on/off    B7/C7
 
 			Print pixel            *, row + 31, col + 31
